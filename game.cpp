a6fdecd72bd4c4650348aecd05d23bc0d5efb60c
Leonard-Cedric <leonardcedriclicayan@gmail.com>
2024-04-28 18:30:30 +0800

    initial

---
+++ game.cpp
@@ -0,0 +1,28 @@
+#include <windows.h>
+#include <iostream>
+
+
+using namespace std;
+
+void gotoXY(int x, int y) {
+    COORD coord; 
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void game(int number[]){
+
+    cout << number[0];
+}
+
+int main() {
+
+    int number[] = {12, 7, 8, 9};
+    game(number);
+
+    // Example usage
+    gotoXY(10, 5);
+    cout << "hello World";
+    return 0;
+}
